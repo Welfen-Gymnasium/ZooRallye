71686c52583d33170cd9ac3568e8dddf5228fbc3
Xeselaro <sese.tailor@gmx.net>
2016-03-29 21:29:58 +0200

    Finishing Lianas

--- mobile/src/main/java/onl/deepspace/zoorallye/AboutActivity.java
+++ mobile/src/main/java/onl/deepspace/zoorallye/AboutActivity.java
@@ -1,9 +1,7 @@
 package onl.deepspace.zoorallye;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.view.ViewOverlay;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 import onl.deepspace.zoorallye.helper.Liane;
@@ -18,17 +16,7 @@ public class AboutActivity extends AppCompatActivity {
         TextView authors = (TextView) findViewById(R.id.about_authors);
         authors.setMovementMethod(android.text.method.LinkMovementMethod.getInstance());
 
-        final View view = findViewById(R.id.about_activity);
-        final ViewOverlay overlay = view.getOverlay();
-        final Liane liane = new Liane();
-
-        view.post(new Runnable() {
-            @Override
-            public void run() {
-                liane.setBounds(view.getWidth() / 2, 0, view.getWidth(), view.getHeight() / 2);
-                overlay.add(liane);
-            }
-        });
+        Liane.addLiane(findViewById(R.id.about_activity));
 
     }
 }
