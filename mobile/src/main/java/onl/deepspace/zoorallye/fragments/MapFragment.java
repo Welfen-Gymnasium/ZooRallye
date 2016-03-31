bfc38a0eb51867356cf8cce9618524c6d4276bc6
Sese Schneider <sese.tailor@gmail.com>
2016-03-31 03:32:51 +0200

    Map with GPS &  transverse liane
    Tons of work

--- mobile/src/main/java/onl/deepspace/zoorallye/fragments/MapFragment.java
+++ mobile/src/main/java/onl/deepspace/zoorallye/fragments/MapFragment.java
@@ -1,26 +1,91 @@
 package onl.deepspace.zoorallye.fragments;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import java.util.zip.Inflater;
+import android.view.ViewOverlay;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import onl.deepspace.zoorallye.R;
+import onl.deepspace.zoorallye.helper.Const;
+import onl.deepspace.zoorallye.helper.GPSTracker;
+import onl.deepspace.zoorallye.helper.Liane;
 
 /**
  * Created by Sese on 30.03.2016.
  */
 public class MapFragment extends Fragment {
 
+    GPSTracker gps;
+    View view;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_map, container, false);
+        view = inflater.inflate(R.layout.fragment_map, container, false);
+
+        //Init Lianas
+        Liane.addLiane(view);
+
+        //Map
+        final ImageView map = (ImageView) view.findViewById(R.id.fragment_map_map);
+        //map.setZoom(1.2f); //not working... Why? //TODO Make map-default-zoom work
+
+        //GPS
+        gps = new GPSTracker(getContext());
+
+        if(gps.canGetLocation()){
+            Toast.makeText(getContext(), gps.getLongitude() + " " + gps.getLatitude(), Toast.LENGTH_LONG).show();
+            final double latitude = gps.getLongitude();
+            final double longitude = gps.getLatitude();
+
+            //WhereAmI overlay AFTER Inflation
+            final ViewOverlay overlay = map.getOverlay();
+            map.post(new Runnable() {
+                @Override
+                public void run() {
+                    Log.d(Const.LOGTAG, String.valueOf(view.getWidth() + " " + map.getWidth()));
+
+                    double xRange = (Const.maxLongitude -  Const.minLongitude);
+                    double yRange = (Const.maxLatitude - Const.minLatitude);
+                    double xStep = map.getWidth() / xRange;
+                    double yStep =  map.getHeight() / yRange;
+
+                    double userXRange = (longitude - Const.minLongitude);
+                    double userYRange = (Const.maxLatitude - latitude);
+
+                    int xMarker = (int) (userXRange * xStep);
+                    int yMarker = (int) (userYRange * yStep);
+
+                    if(xMarker >= 0 && xMarker <= view.getWidth() && yMarker >= 0 && yMarker <= view.getHeight()) {
+
+                        Drawable marker = map.getResources().getDrawable(R.drawable.ic_map_marker);
+                        marker.setBounds(xMarker - 50, yMarker - 110, xMarker + 100 - 50, yMarker + 110 - 110); //Better understanding, 100*110 icon with movments
+                        overlay.add(marker);
+                    }
+                    else{
+                        Snackbar.make(view, view.getResources().getString(R.string.outside_zoo), Snackbar.LENGTH_LONG).show();
+                    }
+                }
+            });
+        }
+        else{
+            gps.showSettingsAlert();
+        }
 
         return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        gps.stopUsingGPS();
     }
 }
--- mobile/src/main/java/onl/deepspace/zoorallye/helper/Const.java
+++ mobile/src/main/java/onl/deepspace/zoorallye/helper/Const.java
@@ -8,7 +8,12 @@ package onl.deepspace.zoorallye.helper;
 public class Const {
     final public static String LOGTAG = "ZooRallye";
 
-    final public static String QuestionsAPI = "http://api.deepspace.onl/zoorallye/questions.php";
-    final public static String QuestionsAPI_token = "token";
-    final public static String API_TOKEN = "x9j7qtRUOrGGg5fxHakK1sp6T4h9JbJjL44iulAKA8HPbbmBWd2QUEQpFD35i";
+    public static final String QuestionsAPI = "http://api.deepspace.onl/zoorallye/questions.php";
+    public static final String QuestionsAPI_token = "token";
+    public static final String API_TOKEN = "x9j7qtRUOrGGg5fxHakK1sp6T4h9JbJjL44iulAKA8HPbbmBWd2QUEQpFD35i";
+
+    public static final double minLatitude = 48.343800;
+    public static final double minLongitude = 10.912091;
+    public static final double maxLatitude = 48.349773;
+    public static final double maxLongitude = 10.918707;
 }
