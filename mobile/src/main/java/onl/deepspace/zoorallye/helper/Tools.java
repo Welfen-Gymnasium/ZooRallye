ec6c082a2df0e63fc84ec351b6743e832f6e99fe
Sese Schneider <sese.tailor@gmail.com>
2016-04-18 17:24:47 +0200

    DataFetcher --> httpRequests

--- mobile/src/main/java/onl/deepspace/zoorallye/helper/Tools.java
+++ mobile/src/main/java/onl/deepspace/zoorallye/helper/Tools.java
@@ -2,33 +2,13 @@ package onl.deepspace.zoorallye.helper;
 
 import android.app.Activity;
 import android.content.pm.PackageManager;
-import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.annotation.StringRes;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
-import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
-import android.widget.Toast;
-
-import com.google.android.gms.auth.api.Auth;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.drive.Permission;
-import com.google.android.gms.games.Games;
-import com.google.android.gms.games.achievement.Achievement;
-
-import onl.deepspace.zoorallye.MainActivity;
-import onl.deepspace.zoorallye.R;
-import onl.deepspace.zoorallye.helper.activities.AppCompatAchievementActivity;
 
 /**
  * Created by Sese on 03.04.2016.
